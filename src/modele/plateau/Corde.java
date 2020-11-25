04d94f0a56e616fd511e301bd4c638f41980e806
COMO ADRIEN p1709079 <adrien.como@etu.univ-lyon1.fr>
2020-11-25 16:03:35 +0100

    Upload New File

---
+++ src/modele/plateau/Corde.java
@@ -0,0 +1,9 @@
+package modele.plateau;
+
+public class Corde extends EntiteDynamique {
+    public Corde(Jeu _jeu) { super(_jeu); }
+
+    public boolean peutEtreEcrase() { return true; }
+    public boolean peutServirDeSupport() { return false; }
+    public boolean peutPermettreDeMonterDescendre() { return true; };
+}
