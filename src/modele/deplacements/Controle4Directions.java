eff23cb4e362411f89ddd73ce4088ff10e4b8382
COMO ADRIEN p1709079 <adrien.como@etu.univ-lyon1.fr>
2020-12-16 15:39:59 +0100

    Update Controle4Directions.java

--- src/modele/deplacements/Controle4Directions.java
+++ src/modele/deplacements/Controle4Directions.java
@@ -12,14 +12,30 @@ public class Controle4Directions extends RealisateurDeDeplacement {
     private static Controle4Directions c4d;
 
     public static Controle4Directions getInstance() {
-        if (c4d == null) {
+        if (c4d == null)
             c4d = new Controle4Directions();
-        }
         return c4d;
     }
 
     public void setDirectionCourante(Direction _directionCourante) {
         directionCourante = _directionCourante;
+    }
+
+    public void setRamassable(){
+        for (EntiteDynamique e : lstEntitesDynamiques) {
+            if(((Heros)e).getRamassable()) {
+                Entite eDroite = e.regarderDansLaDirection(Direction.droite);
+                Entite eGauche = e.regarderDansLaDirection(Direction.gauche);
+                if((eDroite == null || eDroite instanceof Vide) && ((Heros)e).solDirectionHeros) {
+                    e.getJeu().deposerRamassable(e, true);
+                    ((Heros) e).setRamassable(false);
+                }
+                else if((eGauche == null || eGauche instanceof Vide) && !((Heros)e).solDirectionHeros) {
+                    e.getJeu().deposerRamassable(e, false);
+                    ((Heros) e).setRamassable(false);
+                }
+            }
+        }
     }
 
     public boolean realisationDeplacementSurColonne(boolean dir){
@@ -37,59 +53,52 @@ public class Controle4Directions extends RealisateurDeDeplacement {
     public boolean realiserDeplacement() {
         boolean ret = false;
         checkForSmick();
+
         for (EntiteDynamique e : lstEntitesDynamiques) {
 
              if(e.getPrec() instanceof Bombe){
-                e.setEntitePrec(null);
-                //e.getJeu().getGameplay().incrementerScore(); // a faire
+                 e.setEntitePrec(null);
+                 ((Heros)e).incrementerScore(10);
             }
 
             if(e.getPrec() instanceof Ramassable){
                 if(!((Heros)e).getRamassable())
                     e.setEntitePrec(null);
                     ((Heros)e).setRamassable(true);
+                    ((Heros)e).incrementerScore(5);
             }
 
             if (directionCourante != null)
                 switch (directionCourante) {
-
-
-                    case gauche :
+                    case gauche:
                         Entite eGauche = e.regarderDansLaDirection(Direction.gauche);
-                        if(eGauche == null || (eGauche != null && eGauche.peutEtreEcrase()))
-                            if (e.avancerDirectionChoisie(Direction.gauche))
-                                ret = true;
-                    break;
+                        if (eGauche == null || (eGauche != null && eGauche.peutEtreEcrase()))
+                            ((Heros) e).solDirectionHeros = false;
+                        if (e.avancerDirectionChoisie(Direction.gauche))
+                            ret = true;
+                        break;
 
-                     case droite :
-                        //Entite eDroit = e.regarderDansLaDirection(Direction.droite);
-                        //if(eDroit == null || eDroit.peutEtreEcrase())
-                            if (e.avancerDirectionChoisie(Direction.droite))
-                                ret = true;
-                    break;
-
+                    case droite:
+                        Entite eDroite = e.regarderDansLaDirection(Direction.droite);
+                        if (eDroite == null || (eDroite != null && eDroite.peutEtreEcrase()))
+                            ((Heros) e).solDirectionHeros = true;
+                        if (e.avancerDirectionChoisie(Direction.droite))
+                            ret = true;
+                        break;
 
                     case haut:
-                        // on ne peut pas sauter sans prendre appui
-                        // (attention, test d'appui réalisé à partir de la position courante, si la gravité à été appliquée, il ne s'agit pas de la position affichée, amélioration possible)
-                        Entite eBas = e.regarderDansLaDirection(Direction.bas);
                         Entite eHaut = e.regarderDansLaDirection(Direction.haut);
-
-                        if(eHaut == null || ( eHaut != null  &&  (eHaut.peutServirDeSupport() || eHaut.peutPermettreDeMonterDescendre()))) {
+                        if (eHaut == null || (eHaut != null && (eHaut.peutServirDeSupport() || eHaut.peutPermettreDeMonterDescendre())))
                             if (e.avancerDirectionChoisie(Direction.haut))
                                 ret = true;
-                        }
+                        break;
 
-                    break;
-
-                    case bas :
-                        Entite _eBas = e.regarderDansLaDirection(Direction.bas);
-
-                        if(_eBas == null || (_eBas != null && (_eBas.peutServirDeSupport() || _eBas.peutPermettreDeMonterDescendre()))) {
+                    case bas:
+                        Entite eBas = e.regarderDansLaDirection(Direction.bas);
+                        if (eBas == null || (eBas != null && (eBas.peutServirDeSupport() || eBas.peutPermettreDeMonterDescendre())))
                             if (e.avancerDirectionChoisie(Direction.bas))
                                 ret = true;
-                        }
-                    break;
+                        break;
                 }
         }
         return ret;
